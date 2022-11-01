797fffb8f730ce88c5e743b02b5d8a510fe6e2e7
Junaid69 <junaidqureshi091@gmail.com>
2022-11-01 23:25:37 +0530

    Junaid69

--- MeatandGreet/wsgi.py
+++ MeatandGreet/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for BarStockExchange project.
+WSGI config for MeatandGreet project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BarStockExchange.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MeatandGreet.settings')
 
 application = get_wsgi_application()
